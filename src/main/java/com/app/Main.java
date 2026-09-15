0f3647fae2411dfd406f1b179226551165f7a370
michab17 <michajbernhard@gmail.com>
2026-09-14 21:03:03 -0600

    Get height and width of image as a test

--- src/main/java/com/app/Main.java
+++ src/main/java/com/app/Main.java
@@ -1,7 +1,29 @@
 package com.app;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        BufferedImage image;
+
+        try {
+            image = ImageIO.read(new File("C:\\Users\\micha\\Documents\\Java\\site-screenshot.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int imageHeight = 0;
+        int imageWidth = 0;
+
+        if (image != null) {
+            imageHeight = image.getHeight();
+            imageWidth = image.getWidth();
+        }
+
+        System.out.println("This is the image height " + imageHeight);
+        System.out.println("This is the image width " + imageWidth);
     }
 }
